53bb48b797d1d934f2ac578e7f90bdd85017ffa8
Aryansh-kr001 <140975421+Aryansh-kr001@users.noreply.github.com>
2023-07-30 23:59:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="intro">
+      <b>
+        <i>
+        <h1>Aryansh Kumar</h1>
+        </i>
+      </b>
+    </div>
+  );
+}
+
+export default App;
